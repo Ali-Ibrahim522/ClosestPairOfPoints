b94d268e357cadd9ccb4a180565ad4ee646087cc
Ali-Ibrahim522 <52763138+Ali-Ibrahim522@users.noreply.github.com>
2022-05-12 10:25:59 -0700

    looking at two closest pairs

--- src/ClosestPairOfPoints.java
+++ src/ClosestPairOfPoints.java
@@ -1,6 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
+
+import static java.util.Collections.sort;
 
 public class ClosestPairOfPoints {
     private final PointXComparator sortX;
@@ -11,57 +16,138 @@ public class ClosestPairOfPoints {
     public ClosestPairOfPoints(String fileName) {
         sortX = new PointXComparator();
         sortY = new PointYComparator();
+        build10k();
         this.fileName = fileName;
+    }
+
+    public void build10k() {
+        try {
+            PrintWriter writer = new PrintWriter("10kdata.txt", StandardCharsets.UTF_8);
+            writer.println("10000");
+            Random rand = new Random();
+            for (int i = 0; i < 10000; i++) {
+                writer.println(rand.nextDouble(300) + " " + rand.nextDouble(300));
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void findClosestPair() {
         initializeData();
         closestPair(0, points.length - 1);
+        bruteForce();
     }
 
-    public double closestPair(int start, int end) {
-        double delta;
+    private void bruteForce() {
+        ArrayList<Double> distances = new ArrayList<>();
+        for (int i = 0; i < points.length - 1; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                distances.add(Math.sqrt(Math.pow((points[i].getX() - points[j].getX()), 2) +
+                        Math.pow((points[i].getY() - points[j].getY()), 2)));
+            }
+        }
+        sort(distances);
+        for (int i = 0; i < 2; i++) {
+            System.out.printf("%.4f %n", distances.get(i));
+        }
+    }
+
+    public double[] closestPair(int start, int end) {
+        double[] delta = new double[2];
+        delta[0] = Double.MAX_VALUE;
+        delta[1] = Double.MAX_VALUE;
         double distance;
         if (end - start < 3) {
-            delta = Integer.MAX_VALUE;
             for (int i = start; i <= end - 1; i++) {
                 for (int j = i + 1; j <= end; j++) {
-                    distance = Math.sqrt(Math.pow((points[i].getX() - points[j].getX()), 2) + Math.pow((points[i].getY() - points[j].getY()), 2));
-                    if (distance < delta) {
-                        delta = distance;
+                    distance = Math.sqrt(Math.pow((points[i].getX() - points[j].getX()), 2) +
+                            Math.pow((points[i].getY() - points[j].getY()), 2));
+                    if (distance < delta[0]) {
+                        double temp = delta[0];
+                        delta[0] = distance;
+                        delta[1] = temp;
+                    } else if (distance < delta[1] && Double.compare(distance, delta[0]) != 0) {
+                        delta[1] = distance;
                     }
+                    Arrays.sort(delta);
                 }
             }
+            System.out.println("[" + delta[0] + ", " + "]");
         } else {
             int mid = (start + end) / 2;
-            double closestLeft = closestPair(start, mid);
-            double closestRight = closestPair(mid + 1, end);
+            double[] closestLeft = closestPair(start, mid);
+            double[] closestRight = closestPair(mid + 1, end);
             double l = points[mid].getX();
-            delta = Math.min(closestLeft, closestRight);
+            for (int i = 0; i < 2; i++) {
+                if (closestLeft[i] < delta[0]) {
+                    double temp = delta[0];
+                    delta[0] = closestLeft[i];
+                    delta[1] = temp;
+                } else if (closestLeft[i] < delta[1] && Double.compare(closestLeft[i], delta[0]) != 0) delta[1] = closestLeft[i];
+                Arrays.sort(delta);
+            }
+            for (int i = 0; i < 2; i++) {
+                if (closestRight[i] < delta[0]) {
+                    double temp = delta[0];
+                    delta[0] = closestRight[i];
+                    delta[1] = temp;
+                } else if (closestRight[i] < delta[1] && Double.compare(closestRight[i], delta[0]) != 0) delta[1] = closestRight[i];
+                Arrays.sort(delta);
+            }
             ArrayList<Point> deltaRange = new ArrayList<>();
             for (int i = start; i <= end; i++) {
-                if (points[i].getX() <= l + delta && points[i].getX() >= l - delta) {
+                if (points[i].getX() <= l + delta[0] && points[i].getX() >= l - delta[0]) {
                     deltaRange.add(points[i]);
                 }
             }
+            double nextDelta = delta[1];
             deltaRange.sort(sortY);
             Point from;
             Point to;
             for (int i = 0; i < deltaRange.size() - 1; i++) {
                 from = deltaRange.get(i);
-                //System.out.println(from.getX() + ", " + from.getY());
                 for (int j = i + 1; j < Math.min(7, deltaRange.size()); j++) {
                     to = deltaRange.get(j);
-                    distance = Math.sqrt(Math.pow((from.getX() - to.getX()), 2) + Math.pow((from.getY() - to.getY()), 2));
-                    //System.out.println("(" + from.getX() + ", " + from.getY() + ") " + distance + " (" + to.getX() + ", " + to.getY() + ")");
-                    if (distance < delta) {
-                        //System.out.println("New closest!!");
-                        delta = distance;
+                    distance = Math.sqrt(Math.pow((from.getX() - to.getX()), 2) +
+                            Math.pow((from.getY() - to.getY()), 2));
+                    if (distance < delta[0]) {
+                        double temp = delta[0];
+                        delta[0] = distance;
+                        delta[1] = temp;
+                    } else if (distance < delta[1] && Double.compare(distance, delta[0]) != 0) {
+                        delta[1] = distance;
                     }
+                    Arrays.sort(delta);
+                }
+            }
+            deltaRange = new ArrayList<>();
+            for (int i = start; i <= end; i++) {
+                if (points[i].getX() <= l + nextDelta && points[i].getX() >= l - nextDelta) {
+                    deltaRange.add(points[i]);
+                }
+            }
+            deltaRange.sort(sortY);
+            for (int i = 0; i < deltaRange.size() - 1; i++) {
+                from = deltaRange.get(i);
+                for (int j = i + 1; j < Math.min(7, deltaRange.size()); j++) {
+                    to = deltaRange.get(j);
+                    distance = Math.sqrt(Math.pow((from.getX() - to.getX()), 2) +
+                            Math.pow((from.getY() - to.getY()), 2));
+                    if (distance < delta[0]) {
+                        double temp = delta[0];
+                        delta[0] = distance;
+                        delta[1] = temp;
+                    } else if (distance < delta[1] && Double.compare(distance, delta[0]) != 0) {
+                        delta[1] = distance;
+                    }
+                    Arrays.sort(delta);
                 }
             }
         }
-        System.out.printf("D[" + start + "," + end + "]: " + "%.4f %n", delta);
+        System.out.printf("D[" + start + "," + end + "]: " + "%.4f", delta[0]);
+        System.out.println(", " + delta[1]);
         return delta;
     }
 
@@ -75,6 +161,7 @@ public class ClosestPairOfPoints {
                 points[i] = new Point(in.nextDouble(), in.nextDouble());
             }
             Arrays.sort(points, sortX);
+            in.close();
         } catch (FileNotFoundException ex) {
             System.out.println("File not found");
         }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        ClosestPairOfPoints cpop = new ClosestPairOfPoints("program2data.txt");
+        ClosestPairOfPoints cpop = new ClosestPairOfPoints("10kdata.txt");
         cpop.findClosestPair();
     }
 }
